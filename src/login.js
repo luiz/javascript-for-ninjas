c4f9a95b22a5f215e33472b59022f1af67b6b22a
Caires Vinicius <cairesvinicius@caires-viniciuss-macbook.local>
2009-05-13 20:54:10 -0300

    view de login e de cadastro

---
+++ src/login.js
@@ -0,0 +1,71 @@
+/*
+  TODO All  javascript stuffs.
+*/
+$(function(){  
+  $('div#login').show();
+  var login = $('input#username');
+  var password = $('input#password');
+  login.focus();
+  validate.login(login);
+  validate.password(password);
+});
+
+/*
+  Coloque aqui as views
+*/
+var viewsId = []
+viewsId[0] = 'login';
+viewsId[1] = 'register';
+
+function hideViewsFor(view){
+  $(viewsId).each(function(c,e){
+    if(e != view){
+      $('div#'+e).hide();
+    }
+  });
+}
+
+var validate = {
+  login : function( loginName ){
+     $(loginName).blur(function(){
+      if(loginName.val() != 'secret'){
+        loginName.next('error').html('');
+        if(loginName.val() == ''){
+          loginName.next('error').fadeIn('slow').text('O campo login não pode ser vazio');
+        }else{
+          loginName.next('error').fadeIn('slow').text('Usuario não existe');
+        }
+      }else{
+        loginName.next('error').fadeOut();
+      }
+    });
+  },
+  
+  password: function(passwordText){
+    $(passwordText).blur(function(){
+      if( passwordText.val() == '' ){
+        passwordText.next('error').fadeIn('slow').text('O campo senha não pode ser vazio');
+      }else{
+        passwordText.next('error').fadeOut();
+      }
+    });
+  }, 
+}
+
+function buildRegisterView(){
+  hideViewsFor('register');
+  setTimeout(function() {
+    $('div#register').show('slow');
+    $('a#registerNewUser').hide();
+    $('a#linkToLogin').show('slow');
+  }, 1000);
+}
+
+function buildLoginView(){
+  hideViewsFor('login');
+  setTimeout(function() {
+    $('div#login').show('slow');
+    $('a#linkToLogin').hide('slow');
+    $('a#registerNewUser').show();
+  }, 1000);
+}
--- src/ninja.js
+++
@@ -1,8 +0,0 @@
-/*
-  TODO All  javascript stuffs.
-*/
-
-$(function(){
-  if($('div#section-header'))
-    $('div#section-header').html('success');
-});
